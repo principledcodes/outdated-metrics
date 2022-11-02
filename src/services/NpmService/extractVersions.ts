39ea8d9dcd24000db8ddcfebeb6b32d3aef275e3
craigs <hi@craigs.io>
2022-11-02 19:35:01 +1100

    widen regex to filter out all versions with labels

--- src/services/NpmService/extractVersions.ts
+++ src/services/NpmService/extractVersions.ts
@@ -1,7 +1,7 @@
 import { Versions } from '../../types'
 
 export const extractVersions = (versions: Record<string, string>): Versions => {
-  const filter = /-alpha|-beta|-dev/
+  const filter = /[\d.]+-\D+.*/
   const filtered = Object.entries(versions).filter(([key]) => !filter.test(key))
 
   return Object.fromEntries(filtered.map(([key, value]) => [key, new Date(value)]))
