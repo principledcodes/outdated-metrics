c6b64c5e65ad0857d2697ce7447a7fefe736a99c
antklim <antklim@gmail.com>
2022-11-04 07:52:06 +1100

    Added CLI check module

---
+++ src/cli/check.ts
@@ -0,0 +1,20 @@
+import { BaseOptions } from "../cli";
+
+const checkMaxDate = (maxDate: string | null): boolean => {
+  if (maxDate != null) {
+    const maxDateEpoch = Date.parse(maxDate)
+
+    if (isNaN(maxDateEpoch)) {
+      throw new Error(`Provided date '${maxDate}' is not valid. ` +
+        'Please enter date in YYYY-MM-DD format.')
+    }
+  }
+
+  return true
+}
+
+export const check = (options: BaseOptions): boolean => {
+  const { maxDate } = options
+
+  return checkMaxDate(maxDate)
+}
