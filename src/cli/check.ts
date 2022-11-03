bd3d434ce77ce5da856762854a66374d98987f9e
antklim <antklim@gmail.com>
2022-11-04 08:13:27 +1100

    Refactoring

--- src/cli/check.ts
+++ src/cli/check.ts
@@ -1,13 +1,13 @@
 import { BaseOptions } from './options'
 
 const checkMaxDate = (maxDate: string | null): boolean => {
-  if (maxDate != null) {
-    const maxDateEpoch = Date.parse(maxDate)
+  if (maxDate == null) return true
 
-    if (isNaN(maxDateEpoch)) {
-      throw new Error(`Provided date '${maxDate}' is not valid. ` +
-        'Please enter date in YYYY-MM-DD format.')
-    }
+  const maxDateEpoch = Date.parse(maxDate)
+
+  if (isNaN(maxDateEpoch)) {
+    throw new Error(`Provided date '${maxDate}' is not valid. ` +
+      'Please enter date in YYYY-MM-DD format.')
   }
 
   return true
