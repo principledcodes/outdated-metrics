a8ada545f592498039c6d58f0c77e17cd7337669
antklim <antklim@gmail.com>
2022-11-16 22:45:32 +1100

    using pkgContent in file and url loaders

--- src/services/LoadService/url.ts
+++ src/services/LoadService/url.ts
@@ -1,15 +1,10 @@
 import fetch from 'isomorphic-fetch'
+import { pkgContent } from './pkgContent'
 import { Loader } from '../../types'
 
 export const url: Loader = async url => {
   const response = await fetch(url)
   const data = await response.json()
 
-  const { dependencies, devDependencies } = data
-
-  return {
-    dependencies: Object.keys(dependencies),
-    devDependencies: Object.keys(devDependencies),
-    versions: { ...dependencies, ...devDependencies }
-  }
+  return pkgContent(data)
 }
