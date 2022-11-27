7d8c32a6db2e55b5fb8dcb48488f4fdb267ca5f6
antklim <antklim@gmail.com>
2022-11-27 17:48:13 +1100

    refactored filter

--- src/services/ReportService/filter.ts
+++ src/services/ReportService/filter.ts
@@ -6,28 +6,12 @@ type Filter = (
 ) => Partial<SummaryMetrics>
 
 export const filter: Filter = (metrics, options) => {
-  const {
-    averageDays,
-    dirtyPackages,
-    percentage,
-    totalDays,
-    totalPackages
-  } = metrics
-
   const filtered: Partial<SummaryMetrics> = {}
 
-  if (options.averageDays) filtered.averageDays = averageDays
-  if (options.dirtyPackages) filtered.dirtyPackages = dirtyPackages
-  if (options.percentage) filtered.percentage = percentage
-  if (options.totalDays) filtered.totalDays = totalDays
-  if (options.totalPackages) filtered.totalPackages = totalPackages
+  let key: keyof SummaryMetrics
+  for (key in options) {
+    if (options[key]) filtered[key] = metrics[key]
+  }
 
   return filtered
 }
-
-// Object.entries(options)
-// .filter(([key, val]) => val)
-// .reduce((acc, [key]) => {
-//   acc[key] = metrics[key]
-//   return acc
-// }, {})
